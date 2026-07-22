656076fa2b58cd258a0ce8cf80e05b446d1851f7
Dylan Karu <300696660+dylankaru@users.noreply.github.com>
2026-07-22 12:18:18 +0800

    Initial Commit

---
+++ air/airApp.swift
@@ -0,0 +1,32 @@
+//
+//  airApp.swift
+//  air
+//
+//  Created by Dylan Karunanayake on 22/7/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct airApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ airTests/airTests.swift
@@ -0,0 +1,19 @@
+//
+//  airTests.swift
+//  airTests
+//
+//  Created by Dylan Karunanayake on 22/7/2026.
+//
+
+import Testing
+@testable import air
+
+struct airTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
